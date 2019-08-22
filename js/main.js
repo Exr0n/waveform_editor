f7ce382b1127058371cc338a6d6223e1bb917f74
minecire <unconfigured@null.spigotmc.org>
2019-08-21 22:39:32 -0400

    kommit

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ var mouseX, mouseY;
 var clicking;
 var rClicking;
 var dropDownSelected = -1;
+var scroll = 0;
 
 function setMouseCoords(event){
   mouseX = event.clientX;
@@ -27,6 +28,10 @@ function mUp(event){
   else{
     clicking = true;
   }
+}
+
+function mScroll(event){
+  scroll += event.wheelDelta * canvas.width / 3840;
 }
 
 function Rect(x, y, w, h, col, selectable, dds){
@@ -96,8 +101,10 @@ class scale {
 }
 
 function run(){
+  scroll *= (window.innerWidth-3) / canvas.width;
   canvas.width=window.innerWidth-3;
   canvas.height=window.innerHeight-4;
+  
   Rect(0,0,canvas.width,canvas.height, "base", false);
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
@@ -106,26 +113,24 @@ function run(){
   for(var indexOfThreads = 0; indexOfThreads < threads.length+1; indexOfThreads++){
     if(indexOfThreads == threads.length){
       // box with + sign / data
-      if(Rect(canvas.width*0.01,canvas.height*0.1+canvas.height*0.11*indexOfThreads,canvas.width*0.08,canvas.height*0.1, "second", true, false) && clicking){
+      if(Rect(canvas.width*0.01,canvas.height*0.1+canvas.height*0.11*indexOfThreads + scroll,canvas.width*0.08,canvas.height*0.1, "second", true, false) && clicking){
         var s = window.prompt("What do you want to call your new thread?","Thread "+(threads.length+1));
         if(s != null){
           threads.push(new thread([],"Western", s));
         }
       }
       
-      //main box to show music
-      Rect(canvas.width*0.1,canvas.height*0.1+canvas.height*0.11*indexOfThreads,canvas.width*0.89,canvas.height*0.1, "second", false, false);
       ctx.font = Math.min(canvas.width, canvas.height*1.9)*0.07 + "px Arial";
       ctx.fillStyle = textColor;
       
       //add new thread
-      ctx.fillText('+', canvas.width*0.05, canvas.height*0.159+canvas.height*0.11*indexOfThreads);
+      ctx.fillText('+', canvas.width*0.05, canvas.height*0.159+canvas.height*0.11*indexOfThreads + scroll);
       
     }
     else{
 
       // box with data
-      if(Rect(canvas.width*0.01,canvas.height*0.1+canvas.height*0.11*indexOfThreads,canvas.width*0.08,canvas.height*0.1, "second", true, false) && rClicking){
+      if(Rect(canvas.width*0.01,canvas.height*0.1+canvas.height*0.11*indexOfThreads + scroll,canvas.width*0.08,canvas.height*0.1, "second", true, false) && rClicking){
         mouseXTemp = mouseX;
         mouseYTemp = mouseY;
         dropDownSelected = indexOfThreads;
@@ -133,61 +138,61 @@ function run(){
 
       
       // main box to show music
-      Rect(canvas.width*0.1,canvas.height*0.1+canvas.height*0.11*indexOfThreads,canvas.width*0.89,canvas.height*0.1, "second", true, false);
+      Rect(canvas.width*0.1,canvas.height*0.1+canvas.height*0.11*indexOfThreads + scroll,canvas.width*0.89,canvas.height*0.1, "second", true, false);
       for(var indexOfTimes = 0; indexOfTimes < threads[indexOfThreads].times.length+1; indexOfTimes++){
         
         if(indexOfTimes == threads[indexOfThreads].times.length){
           if(threads[indexOfThreads].times.length < 4){
             //box with sub-division
-            if(Rect(canvas.width*0.0125+indexOfTimes*canvas.width*0.02, canvas.height*0.135+canvas.height*0.11*indexOfThreads, canvas.width*0.015, canvas.height*0.025, "base", true, false) && clicking){
+            if(Rect(canvas.width*0.0125+indexOfTimes*canvas.width*0.02, canvas.height*0.135+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.015, canvas.height*0.025, "base", true, false) && clicking){
               var n = window.prompt("How many subdivisions do you want to have?","4");
               if(n != null){
                 threads[indexOfThreads].times.push(n);
               }
             }
             ctx.fillStyle = textColor;
-            ctx.fillText('+', canvas.width*0.02+indexOfTimes*canvas.width*0.02, canvas.height*0.1475+canvas.height*0.11*indexOfThreads);
+            ctx.fillText('+', canvas.width*0.02+indexOfTimes*canvas.width*0.02, canvas.height*0.1475+canvas.height*0.11*indexOfThreads + scroll);
           }
         }
         else{
-          if(Rect(canvas.width*0.0125+indexOfTimes*canvas.width*0.02, canvas.height*0.135+canvas.height*0.11*indexOfThreads, canvas.width*0.015, canvas.height*0.025, "base", true, false) && clicking){
+          if(Rect(canvas.width*0.0125+indexOfTimes*canvas.width*0.02, canvas.height*0.135+canvas.height*0.11*indexOfThreads, canvas.width*0.015, canvas.height*0.025 + scroll, "base", true, false) && clicking){
             var n = window.prompt("What would you like to change this sub-division to?",threads[indexOfThreads].times[indexOfTimes]);
             if(n != null){
               threads[indexOfThreads].times[indexOfTimes] = n;
             }
           }
           ctx.fillStyle = textColor;
-          ctx.fillText(threads[indexOfThreads].times[indexOfTimes], canvas.width*0.02+indexOfTimes*canvas.width*0.02, canvas.height*0.1475+canvas.height*0.11*indexOfThreads);
+          ctx.fillText(threads[indexOfThreads].times[indexOfTimes], canvas.width*0.02+indexOfTimes*canvas.width*0.02, canvas.height*0.1475+canvas.height*0.11*indexOfThreads + scroll);
         }
       }
       
-      if(Rect(canvas.width*0.015, canvas.height*0.105+canvas.height*0.11*indexOfThreads, canvas.width*0.07, canvas.height*0.025, "base", true, false) && clicking){
+      if(Rect(canvas.width*0.015, canvas.height*0.105+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.07, canvas.height*0.025, "base", true, false) && clicking){
         var s = window.prompt("What would you like to change this name to?", threads[indexOfThreads].name);
         if(s != null){
           threads[indexOfThreads].name = s;
         }
       }
       ctx.fillStyle = textColor;
-      ctx.fillText(threads[indexOfThreads].name, canvas.width*0.05, canvas.height*0.1175+canvas.height*0.11*indexOfThreads);
+      ctx.fillText(threads[indexOfThreads].name, canvas.width*0.05, canvas.height*0.1175+canvas.height*0.11*indexOfThreads + scroll);
 
-      if(Rect(canvas.width*0.015, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.0325, canvas.height*0.025, "base", true, false)){
-        Rect(mouseX, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.001, canvas.height*0.025, "second", false, false);
+      if(Rect(canvas.width*0.015, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.0325, canvas.height*0.025, "base", true, false)){
+        Rect(mouseX, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.001, canvas.height*0.025, "second", false, false);
         if(clicking){
           threads[indexOfThreads].volume = ((mouseX - canvas.width*0.015) / canvas.width / 0.0325) * 200;
         }
       }
-      Rect(threads[indexOfThreads].volume / 0.0325 / 100 + canvas.width*0.015, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.001, canvas.height*0.025, "second", false, false);
+      Rect(threads[indexOfThreads].volume * canvas.width * 0.0325 / 200 + canvas.width*0.015, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.001, canvas.height*0.025, "second", false, false);
       
-      if(Rect(canvas.width*0.0525, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.0325, canvas.height*0.025, "base", true, false)){
-        Rect(mouseX, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.001, canvas.height*0.025, "second", false, false);
+      if(Rect(canvas.width*0.0525, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.0325, canvas.height*0.025, "base", true, false)){
+        Rect(mouseX, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.001, canvas.height*0.025, "second", false, false);
         if(clicking){
           threads[indexOfThreads].pan = ((mouseX - canvas.width*0.06875) / canvas.width / 0.0325) * 360;
         }
       }
-      Rect(threads[indexOfThreads].pan / 0.0325 / 180 + canvas.width*0.06875, canvas.height*0.17+canvas.height*0.11*indexOfThreads, canvas.width*0.001, canvas.height*0.025, "second", false, false);
+      Rect(threads[indexOfThreads].pan * 0.0375 * canvas.width / 410 + canvas.width*0.06875, canvas.height*0.17+canvas.height*0.11*indexOfThreads + scroll, canvas.width*0.001, canvas.height*0.025, "second", false, false);
 
       ctx.fillStyle = textColor2;
-      ctx.fillText("v "+Math.round(threads[indexOfThreads].volume), canvas.width*0.03125, canvas.height*0.1825+canvas.height*0.11*indexOfThreads);
+      ctx.fillText("v "+Math.round(threads[indexOfThreads].volume), canvas.width*0.03125, canvas.height*0.1825+canvas.height*0.11*indexOfThreads + scroll);
       ctx.fillText("p "+Math.round(threads[indexOfThreads].pan), canvas.width*0.06875, canvas.height*0.1825+canvas.height*0.11*indexOfThreads);
     }
   }
